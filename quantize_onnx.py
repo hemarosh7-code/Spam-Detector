ffa1cab6a6a53cf6a842dd22aab42e03813f12f0
hemarosh7-code <hemarosh7@gmail.com>
2025-09-09 13:55:32 +0530

    Update quantize_onnx.py

--- quantize_onnx.py
+++ quantize_onnx.py
@@ -1,1 +1,5 @@
+# quantize_onnx.py
+from onnxruntime.quantization import quantize_dynamic, QuantType
+quantize_dynamic("model-onnx/model.onnx", "model-onnx/model_int8.onnx", weight_type=QuantType.QInt8)
+print("Saved quantized ONNX -> model-onnx/model_int8.onnx")
 
